694703d2aea24417ca7348b410fa8759d1ca782d
Maxime Gypteau <maxime.gypteau@gmail.com>
2018-10-15 15:55:49 +0200

    Réalisation quête Angular Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {RouterModule, Routes} from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { MenuComponent } from './menu/menu.component';
+
+const appRoutes:Routes = [
+  {path:"signup", component:SignUpComponent, pathMatch:"full"},
+  {path:"userprofile", component:UserProfileComponent, pathMatch:"full"},
+  {path:"", redirectTo:"UserProfileComponent", pathMatch:"full"}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     SignUpComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {useHash:true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
